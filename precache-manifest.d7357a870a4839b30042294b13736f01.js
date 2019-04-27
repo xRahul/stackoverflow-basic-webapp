c8e8ef411662b8fc21e24eacb282fcfb785e4703
Rahul Jain <rahul_jain@live.in>
2019-04-28 01:55:13 +0530

    Updates

---
+++ precache-manifest.d7357a870a4839b30042294b13736f01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6841ab3d02880ff990d6318caf401de",
+    "url": "/stackoverflow-basic-webapp/index.html"
+  },
+  {
+    "revision": "39fb6a551e0569c26c64",
+    "url": "/stackoverflow-basic-webapp/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "39fb6a551e0569c26c64",
+    "url": "/stackoverflow-basic-webapp/static/js/2.2e4304fa.chunk.js"
+  },
+  {
+    "revision": "65f359ad43141abae6f8",
+    "url": "/stackoverflow-basic-webapp/static/js/main.2bbd684b.chunk.js"
+  },
+  {
+    "revision": "8d3146b773b956ca24ed",
+    "url": "/stackoverflow-basic-webapp/static/js/runtime~main.94452cdd.js"
+  }
+]);
